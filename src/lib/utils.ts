3a28bd9498a5ef4351ee6020311753a3200757fc
Gabriel Oporto <gabyoporto@gmail.com>
2025-04-26 19:36:03 -0300

    refactor: reorganize imports in utils.ts and remove unused formatPrice function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,13 +1,6 @@
-import { clsx, type ClassValue } from "clsx";
+import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
-}
-
-export function formatPrice(price: number): string {
-  return new Intl.NumberFormat("es-AR", {
-    style: "currency",
-    currency: "ARS",
-  }).format(price);
 }
