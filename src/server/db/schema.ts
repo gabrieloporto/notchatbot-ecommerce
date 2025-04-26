92845fc676adbd34f5c2ca5e283f0abed73465cd
Gabriel Oporto <gabyoporto@gmail.com>
2025-04-26 16:44:53 -0300

    refactor: remove checkout and orders API routes, along with related database schema and SQL script

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -12,22 +12,3 @@ export const shippingCosts = pgTable("shipping_costs", {
   postalCode: text("postal_code").primaryKey(),
   price: numeric("price", { precision: 10, scale: 2 }).notNull(),
 });
-
-export const orders = pgTable("orders", {
-  id: serial("id").primaryKey(),
-  total: numeric("total", { precision: 10, scale: 2 }).notNull(),
-  postalCode: text("postal_code").notNull(),
-  shippingCost: numeric("shipping_cost", { precision: 10, scale: 2 }).notNull(),
-  createdAt: text("created_at").notNull(),
-});
-
-export const orderItems = pgTable("order_items", {
-  id: serial("id").primaryKey(),
-  orderId: integer("order_id")
-    .notNull()
-    .references(() => orders.id),
-  productId: integer("product_id")
-    .notNull()
-    .references(() => products.id),
-  quantity: integer("quantity").notNull(),
-});
